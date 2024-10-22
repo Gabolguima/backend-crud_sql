90f8ac13b9262210481a332603111015049f9198
Gabriel Oliveira <gabriel.oliguima@gmail.com>
2024-10-22 19:37:13 -0300

    part two of project finalized

--- query.sql
+++ query.sql
@@ -11,3 +11,41 @@ create table usuarios (
     email varchar(50),
     senha varchar(8) not null
 );
+
+-- 2) Cadastrar os dados dos usuários
+
+INSERT INTO
+usuarios (nome, idade, email, senha)
+VALUES (
+        'Aretha Montgomery',
+        30,
+        'augue.id.ante@odioAliquam.com',
+        'a0B13O3L'
+    ),
+    (
+        'Camden H. Bartlett',
+        15,
+        'turpis.vitae.purus@risusDuisa.ca',
+        'p3P96F3Q'
+    ),
+    (
+        'Raja W. Coffey',
+        30,
+        'raja.feugiat@nonummy.com',
+        's5F51T7L'
+    ),
+    (
+        'Elton D. Olsen',
+        29,
+        'auctor@duiFuscediam.edu',
+        'k5X25B0R'
+    ),
+    (
+        'Shelley E. Frederick',
+        20,
+        'raja.feugiat@nonummy.com',
+        'u2D18F6E'
+    );
+    
+-- Apenas verificando se os usuários foram cadastrados
+SELECT * FROM usuarios;
